ad985ff37dfb85a54be858a16e20d36092f6b63a
Estefanía Gordillo <ada_hack143@hackademy.mx>
2022-06-28 15:40:13 -0500

    cuadro_dialogo

--- app/src/main/java/com/administrador/agua/PersonalInfoActivity.java
+++ app/src/main/java/com/administrador/agua/PersonalInfoActivity.java
@@ -1,8 +1,10 @@
 package com.administrador.agua;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
@@ -11,10 +13,15 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class PersonalInfoActivity extends AppCompatActivity {
 
     private Button btnRegister;
+    private TextView txtvNombre;
+    private TextView txtvApPat;
+    private TextView txtvApMat;
+    private TextView txtvTel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +30,26 @@ public class PersonalInfoActivity extends AppCompatActivity {
         setTitle("Información personal");
 
         btnRegister = findViewById(R.id.btn_register);
+        txtvNombre = findViewById(R.id.edtNombre);
+        txtvApPat = findViewById(R.id.edtApPat);
+        txtvApMat = findViewById(R.id.edtApMat);
+        txtvTel = findViewById(R.id.edtPhone);
+
+        txtvNombre.setBackgroundColor(Color.TRANSPARENT);
+        txtvApPat.setBackgroundColor(Color.TRANSPARENT);
+        txtvApMat.setBackgroundColor(Color.TRANSPARENT);
+        txtvTel.setBackgroundColor(Color.TRANSPARENT);
 
         btnRegister.setBackgroundResource(R.drawable.custom_button); //shape, contorno de esquinas para boton
         btnRegister.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#1E88E5")));
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mostrarDialogo();
+            }
+        });
+
 
     }
     @Override
@@ -48,4 +72,22 @@ public class PersonalInfoActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    private void mostrarDialogo(){
+        AlertDialog.Builder builder = new AlertDialog.Builder( PersonalInfoActivity. this);
+        builder.setTitle("Titulo");
+        builder.setMessage("Solo es una pruena").setPositiveButton("Si", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+            }
+        })
+        .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+
+            }
+        }).setCancelable(false)
+            .show();
+    }
+
 }
